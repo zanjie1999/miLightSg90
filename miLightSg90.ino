a4aa8ba8daa232dd257510aae22ac56709b21eba
Sparkle <zanjie1999@yeah.net>
2023-01-05 21:20:06 +0800

    你甚至可以蓄力开关灯 金属齿轮舵机损耗太大了要蓄力才能开灯

--- miLightSg90.ino
+++ miLightSg90.ino
@@ -3,12 +3,12 @@
  * 咩咩房间的灯sg90舵机控制器
  * Sparkle
  * 20221130
- * v1.0
+ * v2.0
  */
 
 #define BLINKER_WIFI
 #define BLINKER_MIOT_OUTLET
-#define ZYYMEHOMEKIT
+//#define ZYYMEHOMEKIT
 
 #include <Blinker.h>
 #include <Servo.h>
@@ -22,11 +22,21 @@ char auth[] = "yourBlinkerDeviceKey";
 char ssid[] = "wifiSsid";
 char pswd[] = "wifiPwd";
 const int sgIo = 4;
-const int onDeg = 145;
-const int offDeg = 140;
-const int stayDeg = 112;
+// 触发开角度
+const int onDeg = 180;
+// 触发关角度
+const int offDeg = 135;
+// 等待开角度
+const int stay2onDeg = 75;
+// 等待关角度
+const int stay2offDeg = 100;
+// 转动等待时间
+const int degT = 190;
+// 待机超时ms 控制完超时会关掉输出
+const int sgSlpT = 10000;
+unsigned long sgt = 0;
 
-bool oState = false;
+bool oState = true;
 Servo sg;
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
@@ -74,9 +84,15 @@ void dataRead(const String & data) {
 }
 
 void setOnOff(bool isOn) {
+    oState = isOn;
     // 启用输出舵机工作
-    pinMode(sgIo, OUTPUT);
+    if (sgt == 0) {
+      pinMode(sgIo, OUTPUT);
+      sgt = sgSlpT + millis();
+      delay(100);
+    }
     if (isOn) {
+        sg.write(stay2onDeg);
         BUILTIN_SWITCH.print("on");
 //        onoffButton.print("on");
         BlinkerMIOT.powerState("on");
@@ -85,7 +101,10 @@ void setOnOff(bool isOn) {
         cha_switch_on.value.bool_value = true;
         #endif
         sg.write(onDeg);
+        delay(degT);
+        sg.write(stay2offDeg);
     } else {
+        sg.write(stay2offDeg);
         BUILTIN_SWITCH.print("off");
 //        onoffButton.print("off");
         BlinkerMIOT.powerState("off");
@@ -94,102 +113,10 @@ void setOnOff(bool isOn) {
         cha_switch_on.value.bool_value = false;
         #endif
         sg.write(offDeg);
+        delay(degT);
+        sg.write(stay2onDeg);
     }
-    delay(250);
-    sg.write(stayDeg);
-    // 关闭输出给舵机延寿 问就是已经烧了一个舵机的电机
-    delay(200);
-    pinMode(sgIo, INPUT);
     #ifdef ZYYMEHOMEKIT
     homekit_characteristic_notify(&cha_switch_on, cha_switch_on.value);
-    #endif
-    oState = isOn;
-}
-
-void miotPowerState(const String & state) {
-    BLINKER_LOG("need set power state: ", state);
-
-    if (state == BLINKER_CMD_ON) {
-//        digitalWrite(LED_BUILTIN, HIGH);
-        setOnOff(true);
-    }
-    else if (state == BLINKER_CMD_OFF) {
-//        digitalWrite(LED_BUILTIN, LOW);
-        setOnOff(false);
-    }
-}
-
-void setOnOffBtn(const String & state) {
-    BLINKER_LOG("get button state: ", state);
-    setOnOff(state == "on");
-//    Blinker.vibrate();
-}
-
-void setOnOffBtnLed(const String & state) {
-    BLINKER_LOG("get button state: ", state);
-    digitalWrite(LED_BUILTIN, state != "on");
-    onoffButton.print(state);
-//    Blinker.vibrate();
-}
-  
-void setDeg(int32_t value) {
-    pinMode(sgIo, OUTPUT);
-    sg.write(value);
-    Blinker.vibrate();
-}
-
-String summary() {
-    String data = "online, switch: " + STRING_format(oState ? "on" : "off");
-    return data;
-}
-
-#ifdef ZYYMEHOMEKIT
-void cha_switch_on_setter(const homekit_value_t value) {
-    setOnOff(value.bool_value); 
-}
-#endif
-
-void setup() {
-    Serial.begin(115200);
-    BLINKER_DEBUG.stream(Serial);
-
-    // sg90的实际工作范围 这样初始化才能到180度
-    sg.attach(sgIo, 500, 2400);
-    sg.write(stayDeg);
-    delay(200);
-    pinMode(sgIo, INPUT);
-
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    BUILTIN_SWITCH.attach(setOnOffBtn);
-    onoffButton.attach(setOnOffBtnLed);
-    degSlider.attach(setDeg);
-
-    Blinker.begin(auth, ssid, pswd);
-    
-    wifi_set_sleep_type(LIGHT_SLEEP_T);
-    
-    Blinker.attachData(dataRead);
-    Blinker.attachSummary(summary);
-    
-    BlinkerMIOT.attachPowerState(miotPowerState);
-    BlinkerMIOT.attachQuery(miotQuery);
-
-    #ifdef ZYYMEHOMEKIT
-    cha_switch_on.setter = cha_switch_on_setter;
-    arduino_homekit_setup(&config);
-    #endif
-
-    httpUpdater.setup(&httpServer);
-    httpServer.begin();
-
-}
-
-void loop() {
-    Blinker.run();
-    httpServer.handleClient();
-    #ifdef ZYYMEHOMEKIT
-    arduino_homekit_loop();
     #endif
 }
